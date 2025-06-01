4a50428121c75fb45e92c23f7faa386ce4090682
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-01 10:55:34 +0300

    reconfiguration

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'alx_travel_app.listings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
