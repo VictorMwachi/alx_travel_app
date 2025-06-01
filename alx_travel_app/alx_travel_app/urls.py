c13c40aa0e9ee3690ff258a59f29f8c18ad444e5
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-01 21:53:52 +0300

    omit pycache

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Travel App API",
+        default_version='v1',
+        description="API for managing travel-related data",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
+    path('swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui'),
+    path('redoc/',schema_view.with_ui('redoc',cache_timeout=0),name='schema-redoc'),
+    #path('api/', include('alx_travel_app.listings')),
     path('admin/', admin.site.urls),
 ]
